998fead968a9b92165d31e2307c3669b176ec390
mynameiskatchy <karchietrinh@gmail.com>
2021-03-18 01:32:33 -0400

    Flash rainbow on instances of coronavirus

--- script.js
+++ script.js
@@ -6,13 +6,10 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
         if (element.textContent.match(/coronavirus/gi)) {
-            //     const newElement = document.createElement('span')
-            //     newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
-            //         '<span style="background-color: black; color: black;">$1</span>')
-            //     element.replaceWith(newElement)
-            // }
-            //element.textContent = element.textContent.replace(/coronavirus/gi, '█████')
-            element.parentElement.remove()
+            const newElement = document.createElement('span')
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
+                '<span class="rainbow"">$1</span>')
+            element.replaceWith(newElement)
         }
     }
 }
