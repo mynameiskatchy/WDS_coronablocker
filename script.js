f6d2d519233d96eb8bc7e3e00da078d93cea52f1
mynameiskatchy <karchietrinh@gmail.com>
2021-03-18 01:25:58 -0400

    Remove everything related to coronavirus

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function replaceText(element) {
         element.childNodes.forEach(replaceText)
     } else if (element.nodeType === Text.TEXT_NODE) {
         if (element.textContent.match(/coronavirus/gi)) {
-            const newElement = document.createElement('span')
-            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
-                '<span style="background-color: black; color: black;">$1</span>')
-            element.replaceWith(newElement)
+            //     const newElement = document.createElement('span')
+            //     newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
+            //         '<span style="background-color: black; color: black;">$1</span>')
+            //     element.replaceWith(newElement)
+            // }
+            //element.textContent = element.textContent.replace(/coronavirus/gi, '█████')
+            element.parentElement.remove()
         }
-        element.textContent = element.textContent.replace(/coronavirus/gi, '█████')
     }
 }
