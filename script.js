a1cf1bc32d9b79ca17b81946f2a8b140f26c704c
mynameiskatchy <karchietrinh@gmail.com>
2021-03-17 23:04:44 -0400

    Blackout instances of coronavirus

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Hi')
+// start with body, loop thru every element
+replaceText(document.body)
+
+function replaceText(element) {
+    if (element.hasChildNodes()) {
+        element.childNodes.forEach(replaceText)
+    } else if (element.nodeType === Text.TEXT_NODE) {
+        element.textContent = element.textContent.replace(/coronavirus/gi, '█████')
+    }
+}
